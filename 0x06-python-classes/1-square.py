e21e2546330432f0a504ead69a4c4d8aa58e18ab
seblemariam <Seblemariam.sm@gmail.com>
2022-06-21 11:58:07 -0700

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+size
+"""
+
+
+class Square:
+    """Square class with
+    size.
+    """
+
+    def __init__(self, size):
+
+        """initializes the size of variabl
+        """
+        self.__size = size
